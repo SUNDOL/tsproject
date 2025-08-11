33b6485e8765b79fc4854e7523f77487eee730dc
Jin Hwi Hong <methimazole91@gmail.com>
2025-08-11 15:48:38 +0900

    2025-08-11 연습1

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
+
 export class App {
   protected readonly title = signal('client');
+
+  isLoggedIn = signal(false);
+
+  login() {
+    this.isLoggedIn.set(true);
+  }
+
+  logout() {
+    this.isLoggedIn.set(false);
+  }
+
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,3 +4,15 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+initTheme();
+
+function initTheme() {
+  const savedTheme = localStorage.getItem('theme');
+  if (!savedTheme) {
+    const defaultTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.setItem('theme', defaultTheme);
+  } else {
+    document.body.setAttribute('data-bs-theme', savedTheme);
+  }
+}
